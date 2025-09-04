095c522cbd91eba5b3a39a96a12f01f0720a2fa4
federica <fedhere@gmail.com>
2025-09-04 13:21:49 -0400

    Create checkanswers1.py

---
+++ .code/checkanswers1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def check_independent_prob(answer):
+  print("CORRECT" if answer == result else "TRY AGAIN...")
+
+def check_conditional_prob(answer):
+  print("CORRECT" if answer == result else "TRY AGAIN...")
