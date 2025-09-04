3fd35228e7c50685dc7de998254286cfe193bc1e
federica <fedhere@gmail.com>
2025-09-04 14:43:53 -0400

    Update checkanswers1.py

--- .code/checkanswers1.py
+++ .code/checkanswers1.py
@@ -1,7 +1,21 @@
-import numpy as np
 
-def check_independent_prob(answer):
-  print("CORRECT" if answer == result else "TRY AGAIN...")
+def check_independent_intersection(answer):
+  print("CORRECT" if answer == 0 else "TRY AGAIN...")
 
-def check_conditional_prob(answer):
-  print("CORRECT" if answer == result else "TRY AGAIN...")
+def check_independent_probAND(answer):
+  print("CORRECT" if answer == 0.15 else "TRY AGAIN...")
+
+def check_independent_probOR(answer):
+  print("CORRECT" if answer == 0.8 else "TRY AGAIN...")
+
+
+def check_independent_probGIVEN(answer):
+  print("CORRECT" if answer == 0.5 else "TRY AGAIN...")
+
+def check_complement_probOR(answer):
+  print("CORRECT" if answer == 1 else "TRY AGAIN...")
+
+
+def check_complement_probAND(answer):
+  print("CORRECT" if answer == 0 else "TRY AGAIN...")
+
